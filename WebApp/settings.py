7e9c671c0952b602133aa3e9ecd7558e9e620d10
ripazha <gundetidheeraj@gmail.com>
2019-12-07 10:40:52 +0530

    database update

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'WebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlinedb',
+        'HOST': 'localhost',
+        'USER':"root",
+        'PASSWORD': "yourpassword"
     }
 }
+
 
 
 # Password validation
