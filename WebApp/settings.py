788956e9a3b08596714bff3b73824d39c4562f2d
ripazha <gundetidheeraj@gmail.com>
2019-12-07 14:21:02 +0530

    database update

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'WebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlinedb',
+        'HOST': 'localhost',
+        'USER':"root",
+        'PASSWORD': "yourpassword"
     },
     'news': {
         'ENGINE': 'djongo',
