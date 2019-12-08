644b5c74faf37c4c1faf89cac7ec483b64f86af9
Krishna Mohan <krishna.mohan@cleartrip.com>
2019-12-08 09:15:26 +0530

    models

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Airbus.apps.AirbusConfig',
-    'Airbus.models',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
